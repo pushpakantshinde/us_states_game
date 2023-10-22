b5ca7993d683472278bed7693224c18feeb938a0
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-22 21:28:36 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import turtle
+import pandas
 
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"       # Only .gif image be added in turtle
+
+screen.addshape(image)              # It adds new shape to the existing types of shape e.g circle, square, ....
+
+#turtle = turtle.Turtle
+
+turtle.shape(image)
